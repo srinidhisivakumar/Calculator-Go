0547039852b82358d0cf13b1656e658ea9d11df6
AnjNet <anjaninetala64@gmail.com>
2024-11-15 16:24:38 -0500

    Square Function commit by Anjani

--- calculator.go
+++ calculator.go
@@ -1,5 +1,15 @@
 package main
 
+import (
+	"fmt"
+)
+
+// Square function by Anjani
+func Square(a int) int {
+	return a * a
+}
+
 func main() {
-	// Code
+	square := Square(5)
+	fmt.Println("Square:", square)
 }
